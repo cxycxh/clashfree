bafc6ca3e3fc3bbc5be34298bed5e65ec10e36d2
cxy <chenxianyong@sinosoft.com.cn>
2024-09-20 11:54:08 +0800

    update

--- clean_ss.py
+++ clean_ss.py
@@ -1,9 +1,34 @@
 import yaml
 
-with open('clash.yml', 'r',encoding="utf-8") as f:
+with open('clash.yaml', 'r',encoding="utf-8") as f:
     clash_profile = yaml.safe_load(f)
     
 proxies = clash_profile['proxies']
+proxy_groups = clash_profile['proxy-groups']
 
-if not proxies:
-    print("None!")
+ss_names = []
+if proxies:
+    new_proxies = []
+    for proxy in proxies:
+        if proxy['type']=='ss':
+            ss_names.append(proxy['name'])
+        else:
+            new_proxies.append(proxy)
+    clash_profile['proxies'] = new_proxies
+
+for group in proxy_groups:
+    if group['proxies']:
+        new_proxies = []
+        for proxy in group['proxies']:
+            if proxy in ss_names:
+                continue
+            else:
+                new_proxies.append(proxy)
+        group['proxies'] = new_proxies
+
+with open('clash.yml', 'w',encoding="utf-8") as fw:
+    yaml.dump(clash_profile, fw)
+
+
+
+            
